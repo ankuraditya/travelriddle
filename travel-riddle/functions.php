f20053eb7facb9ec978d67dfaae032b398a89e53
ankuraditya <ankuraditya1996@gmail.com>
2026-09-14 00:15:43 +0530

    Remove dark overlay from About banner

--- travel-riddle/functions.php
+++ travel-riddle/functions.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('ABSPATH')) { exit; }
 
-define('TRAVEL_RIDDLE_VERSION', '1.3.1');
+define('TRAVEL_RIDDLE_VERSION', '1.3.2');
 
 function travel_riddle_setup(): void {
     load_theme_textdomain('travel-riddle', get_template_directory() . '/languages');
